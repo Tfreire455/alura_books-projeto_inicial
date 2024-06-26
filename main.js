591c96e084926f5d1236c600e51eeff672832419
Tfreire455 <thiago1.000@outlook.com>
2024-06-26 17:42:07 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//Requisição de arquivo JSON com FetchAPI
+
+let livros = [];
+const endPointAPI =
+  "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+getBuscarLivrosDaAPI();
+
+async function getBuscarLivrosDaAPI() {
+    const response = await fetch(endPointAPI);
+    livros = await response.json()
+    console.table(livros)
+}
+
+livros.forEach(
+    (livro) => {
+        
+    }
+)
+
+/* <div class="livro">
+<img class="livro__imagens" src="imagens/Vue.js.png" alt="Capa do livro  Vue.js
+Construa aplicações incríveis" />
+<h2 class="livro__titulo">
+Vue.js
+Construa aplicações incríveis
+</h2>
+<p class="livro__descricao">Caio Incau</p>
+<p class="livro__preco" id="preco">R$29,90</p>
+<div class="tags">
+<span class="tag">Front-end</span>
+<span class="tag">Back-end</span>
+</div> */
